6cc709ee7e575ba21f343325c872718eec4bad7e
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-06 21:50:44 +0000

    add navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import './index.css'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={require("../../images/airbnb-logo.png")} />
+        </nav>
+    )
+}
