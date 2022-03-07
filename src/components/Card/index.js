253cd489dc0dd3d33ba9d9543ae4338c3e36b22a
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-07 22:14:48 +0000

    add card styling

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,15 +4,15 @@ import './index.css'
 export default function Card() {
     return (
         <div className="card-div">
-            <img src={require("../../images/katie-zaferes.png")} className="card--image" />
+            <img src={require("../../images/katie-zaferes.png")} className="card-image" />
             <div className="card-stats">
-                <img src={require("../../images/star.png")} />
-                <span>5.0</span>
-                <span>(6) • </span>
+                <img src={require("../../images/star.png")} className="card-star"/>
+                <span className="grey">5.0</span>
+                <span className="grey">(6) • </span>
                 <span>USA</span>
             </div>
             <p>Life Lessons with Katie Zaferes</p>
-            <p>From $136 / person</p>
+            <p><span className="bold">From $136</span> / person</p>
         </div>
     )
 }
