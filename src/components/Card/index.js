60cd25656289c7f7dd48640e59d335ac2dcf84a6
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-07 22:10:17 +0000

    hard code card value

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,6 +3,16 @@ import './index.css'
 
 export default function Card() {
     return (
-        <h1>CARD</h1>
+        <div className="card-div">
+            <img src={require("../../images/katie-zaferes.png")} className="card--image" />
+            <div className="card-stats">
+                <img src={require("../../images/star.png")} />
+                <span>5.0</span>
+                <span>(6) • </span>
+                <span>USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p>From $136 / person</p>
+        </div>
     )
 }
