b9c23e23305ece1a46048850b9958ccfafb6a9c8
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-07 21:59:53 +0000

    style hero element

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,10 +3,10 @@ import './index.css'
 
 export default function Hero() {
     return (
-        <section>
-            <img src={require("../../images/photo-grid.png")} />
-            <h1>Online Experiences</h1>
-            <p>Join unique interactive activities led by 
+        <section className="hero">
+            <img src={require("../../images/photo-grid.png")} className="hero-image" alt=""/>
+            <h1 className="hero-header">Online Experiences</h1>
+            <p className="hero-para">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
         </section>
     )
