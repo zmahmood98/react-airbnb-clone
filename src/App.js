c2e4051eeff275b038f1b05bb8d06f6aaf24a680
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-08 21:21:48 +0000

    add more cards, badges and fix styling

--- src/App.js
+++ src/App.js
@@ -2,18 +2,72 @@ import React from "react"
 import { Navbar, Hero, Card } from './components'
 
 export default function App() {
+
+  const cardData = [
+    {
+        id: 1,
+        title: "Life Lessons with Katie Zaferes",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: "katie-zaferes.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Learn Wedding Photography",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 125,
+        coverImg: "wedding-photography.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "mountain-bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
+    }
+]
+
+const cardWithData = cardData.map(item => {
     return (
+        <Card 
+            key={item.id}
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+            openSpots={item.openSpots}
+        />
+    )
+})
+  
+  return (
       <>
         <Navbar />
         <Hero />
-        <Card 
-          img="katie-zaferes.png"
-          rating="5.0"
-          reviewCount={6}
-          country="USA"
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-        />
+        <section className="cards-list">
+            {cardWithData}
+        </section>
+        
       </>
     )
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,8 +2,17 @@ import React from "react"
 import './index.css'
 
 export default function Card(props) {
+
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card-div">
+            {badgeText && <div className="card-badge">{badgeText}</div>}
             <img src={require(`../../images/${props.img}`)} className="card-image" />
             <div className="card-stats">
                 <img src={require("../../images/star.png")} className="card-star" />
@@ -11,8 +20,8 @@ export default function Card(props) {
                 <span className="grey">({props.reviewCount}) • </span>
                 <span className="grey">{props.country}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card-title">{props.title}</p>
+            <p><span className="bold card-price">From ${props.price}</span> / person</p>
         </div>
     )
 }
